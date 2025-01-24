0d113acd5a8954935b7a6c96d435bea4084110c2
eduroblesc <eduroblesc@outlook.com>
2025-01-24 15:07:00 -0500

    cuadro busqueda, filtro, historico

--- parking_frontend/src/App.jsx
+++ parking_frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistroParqueoPage from "./pages/RegistroParqueoPage";
 import RegistroParqueoDetallePage from "./pages/RegistroParqueoDetallePage";
 import TicketPage from "./pages/TicketPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<RegistroParqueoPage />} />
         <Route path="/registro/:id" element={<RegistroParqueoDetallePage />} />
